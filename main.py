ea212baedec879facf91b7788c807aef5c165cb8
FERNANDO AUGUSTO CAMUSSI <24271866@cfp107_2.local>
2026-04-15 16:53:54 -0300

    commit

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title='MVC Produtos')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
-# Configuração do CORS
+# # Configuração do CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://team-code-7jpa.vercel.app/"], 
+    allow_origins=[
+        "http://localhost:8081",
+        "http://127.0.0.1:8081"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
